35a5e35e5ed3f027b3ebbe554d66c68f32c5ce2d
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2015-01-20 13:03:30 +0100

    Basic syntax check for web api method calls

--- lib/slack-rtmapi.rb
+++ lib/slack-rtmapi.rb
@@ -42,9 +42,10 @@ end
 #
 module SlackWebAPI
   def self.call(options)
-    method = options.fetch(:method)
-    options.delete(:method)
     options.fetch(:token)
+    method = options.fetch(:method)
+    fail unless method.match(/^[a-z]+\.[A-z]+$/)
+    options.delete(:method)
     api_url = options[:slack_api_url] || 'https://slack.com/api'
     req = Net::HTTP.post_form URI(api_url + '/' + method), options
     JSON.parse req.body
